4a69604f3925aaa678855e852ae6f120bfc0ea77
mitinaik <naikrashmi63@yahoo.com>
2015-06-03 20:53:01 -0400

    geometry
    adding script

---
+++ Geometry.java
@@ -0,0 +1,19 @@
+public class Geometry
+{
+   //
+   // print the number of sides of several geometry shapes.
+   //
+   public static void main(String[] args)
+   {
+      int sides=7; //declaration with initialization
+      System.out.println("A heptagon has " + sides + " sides.");
+      
+      sides=10; //assignment statement
+      System.out.println(" A decagon has " + sides + " sides.");
+      
+      sides=12;
+      System.out.println(" A dodecagon has " + sides + " sides.");
+
+   }
+
+}
